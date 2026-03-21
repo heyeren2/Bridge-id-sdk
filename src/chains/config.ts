424839afb826049057f1256743b817bfb3dee293
lee785 <leetriq7@gmail.com>
2026-03-21 11:50:13 -0700

    did some changes

--- src/chains/config.ts
+++ src/chains/config.ts
@@ -6,19 +6,19 @@ export interface ChainConfig {
 
 export const CHAINS: Record<string, ChainConfig> = {
 
-    sepolia: {
+    "ethereum sepolia": {
         chainId: 11155111,
         name: "Sepolia",
         rpcUrl: "https://rpc.sepolia.org",
     },
 
-    base: {
+    "base sepolia": {
         chainId: 84532,
         name: "Base",
         rpcUrl: "https://sepolia.base.org",
     },
 
-    arc: {
+    "arc testnet": {
         chainId: 5042002,
         name: "Arc",
         rpcUrl: "https://rpc.testnet.arc.network",
